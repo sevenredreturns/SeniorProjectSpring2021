041fd6389075b7c78dd60aca93c1a7e775be9f99
Theresa Morris <morris85@students.rowan.edu>
2021-03-15 00:57:48 -0400

    Test functions

--- Node with Mongo/mongoFunctions.js
+++ Node with Mongo/mongoFunctions.js
@@ -1,13 +1,60 @@
 let MongoClient = require('mongodb').MongoClient;
 let url = 'mongodb://localhost:27017';
 
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db('achievement-arena');
-
-    dbo.collection('games').find({}).toArray(function (err, result) {
+function getAllGames() {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
-        console.log(result);
-        db.close();
+        let dbo = db.db('achievement-arena');
+
+        dbo.collection('games').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
     });
-});
+}
+
+function getAllUsers(){
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('achievement-arena');
+
+        dbo.collection('users').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+    });
+}
+
+function getAllAchievements(){
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('achievement-arena');
+
+        dbo.collection('achievements').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+    });
+}
+
+function getAllMessages(){
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        let dbo = db.db('achievement-arena');
+
+        dbo.collection('messages').find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+    });
+}
+
+getAllUsers();
+getAllGames();
+getAllAchievements();
+getAllMessages();
+
