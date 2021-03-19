4231ba9bddeda920ff267de876dfcea68c5820c7
Theresa Morris <morris85@students.rowan.edu>
2021-03-19 18:12:09 -0400

    Starting Mongoose shit

--- Node with Mongo/mongoMongoose.js
+++ Node with Mongo/mongoMongoose.js
@@ -5,8 +5,25 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     // we're connected!
+    console.log("Connected!")
 });
 
 const userSchema = new mongoose.Schema({
 
-})
+});
+
+const gameSchema = new mongoose.Schema({
+
+});
+
+const achievementSchema = new mongoose.Schema({
+
+});
+
+const leaderboardSchema = new mongoose.Schema({
+
+});
+
+const messageSchema = new mongoose.Schema({
+
+});
