7d6b77605a1d27ecf130c665424a84193d5be734
Theresa Morris <morris85@students.rowan.edu>
2021-03-22 00:02:43 -0400

    Workin' on mongoose~

--- Back End/mongoMongoose.js
+++ Back End/mongoMongoose.js
@@ -3,27 +3,50 @@ mongoose.connect('mongodb://localhost', {useNewUrlParser: true, useUnifiedTopolo
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function ()
+{
     // we're connected!
+    const userSchema = new mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        otherProfiles: [
+            {
+                steam: String,
+                gog: String,
+                psn: String,
+                xbox: String
+            }
+        ]
+    });
+
+    userSchema.plugin(require('mongoose-autopopulate'));
+
+    const gameSchema = new mongoose.Schema({
+        name: String,
+        steamID: String,
+        gogID: String,
+        system: String
+    });
+
+    const achievementSchema = new mongoose.Schema({
+        game: String,
+        achievements: [
+            {
+                title: String,
+                description: String,
+                points: Number
+            }
+        ]
+    });
+
+    const leaderboardSchema = new mongoose.Schema({
+
+    });
+
+    const messageSchema = new mongoose.Schema({
+
+    });
     console.log("Connected!")
 });
 
-const userSchema = new mongoose.Schema({
 
-});
-
-const gameSchema = new mongoose.Schema({
-
-});
-
-const achievementSchema = new mongoose.Schema({
-
-});
-
-const leaderboardSchema = new mongoose.Schema({
-
-});
-
-const messageSchema = new mongoose.Schema({
-    
-});
