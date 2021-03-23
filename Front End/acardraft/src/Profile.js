7593647593ab64e2acb1cc6fdc4fdb21fcee9d57
Ian Campbell <iandcampbell94@gmail.com>
2021-03-22 21:45:08 -0400

    Basic front end, server with react and some dom elements sprinkled in

---
+++ Front End/acardraft/src/Profile.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Link, Route, useParams, useRouteMatch } from "react-router-dom";
+
+const TrophyRoom = () => {
+
+  return (
+    <div>
+      <h3>Trophy Room</h3>
+    </div>
+  );
+}
+
+const Profile = () => {
+  const { url, path } = useRouteMatch();
+
+  return (
+    <div>
+    <ul>
+      <div class = "container">
+        <p>Info 1</p>
+      </div>
+      <div class = "container">
+        <p>Info 2</p>
+      </div>
+      <div class = "container">
+        <p>Info 3</p>
+      </div>
+      <div>
+        <li>
+          <Link to={`${url}/trophyroom`}>Trophy Room</Link>
+        </li>
+      </div>
+    </ul>
+    <Route path = {`${path}/trophyroom`}>
+      <TrophyRoom />
+    </Route>
+    </div>
+  );
+};
+
+export default Profile
