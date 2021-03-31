f28f0f6f9b3f1903fff84ea3eff712c7bc40d596
Theresa Morris <morris85@students.rowan.edu>
2021-03-30 23:52:15 -0400

    Achievement router and model

--- Back End/app/routes/game.router.js
+++ Back End/app/routes/game.router.js
@@ -3,6 +3,7 @@ module.exports = function(app) {
 
     app.post('/api/game', games.addGame);
     app.get('/api/game/:id', games.getGameByID);
+    app.get('/api/game/:name', games.getGameByName);
     app.get('/api/game', games.getAllGames);
     app.put('/api/game', games.updateGame);
     app.delete('/app/game/:id', games.deleteGame);
