6d4e6cc3d69c2713c207d2b003870126b6a974fd
Theresa Morris <morris85@students.rowan.edu>
2021-04-16 08:12:18 -0400

    Working on better user import

--- Experimental/AppTest/app/src/models/achievements.model.js
+++ Experimental/AppTest/app/src/models/achievements.model.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const achievementSchema = new mongoose.Schema({
-    game: String,
+    appid: number,
     achievements: [
         {
-            title: String,
+            name: String,
+            displayName: String,
             description: String,
+            icon: String,
+            icongray: String
             points: Number
         }
     ]
