274e45e6e17a46733d4d2b1c030babef7f4886c3
KingMikeXS1 <78938662+KingMikeXS1@users.noreply.github.com>
2021-04-21 18:25:19 -0400

    Update main.js

--- Steam/main.js
+++ Steam/main.js
@@ -1,12 +1,35 @@
 const api = require('steam-js-api')
 
-api.setKey('6B328D41EE66949204BBCEBA81C3852A')
+passport.use(new SteamStrategy({
+    returnURL: 'http://localhost:3000/auth/steam/return',
+    realm: 'http://localhost:3000/',
+    apiKey: '6B328D41EE66949204BBCEBA81C3852A'
+  },
+  function(identifier, profile, done) {
+    User.findByOpenID({ openId: identifier }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+app.get('/auth/steam',
+  passport.authenticate('steam'),
+  function(req, res) {
+    // The request will be redirected to Steam for authentication, so
+    // this function will not be called.
+  });
 
-steamID = ''
+app.get('/auth/steam/return',
+  passport.authenticate('steam', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+api.setKey('6B328D41EE66949204BBCEBA81C3852A')
+steamID = user.findByOpenID
 appID = []
 moreInfo = true // Provide more info (name, links)
 
-// Alternatively, use a Promise
 api.getOwnedGames(steamID, appID, moreInfo).then(result => {
     console.log(result.data.games[0])
 }).catch(console.error)
