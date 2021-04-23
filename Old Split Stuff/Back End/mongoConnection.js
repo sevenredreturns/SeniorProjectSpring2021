df73448b4a75a328fdef4a42d9412367c226a5bc
ZachKz <78938720+ZachKz@users.noreply.github.com>
2021-04-22 21:52:58 -0400

    Removed Old Demos and misc folders
    Pre deletes for gluing the project together

--- Old Split Stuff/Back End/mongoConnection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/achievement-arena', {useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-
-module.exports = db
--- Old Split Stuff/Experimental/AppTest/app/src/models/message.model.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const messageSchema = new mongoose.Schema({});
-
-module.exports = mongoose.model('message', messageSchema)
