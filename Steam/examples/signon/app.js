7db51a7780ca1485a4e1b0088b1ebd228293febb
Michael Slutsky <61202543+KingMikeXS@users.noreply.github.com>
2021-05-05 21:38:14 -0400

    deleted everything I added.

--- Steam/examples/signon/app.js
+++ Steam/examples/signon/app.js
@@ -6,8 +6,6 @@ var express = require('express')
   , util = require('util')
   , session = require('express-session')
   , SteamStrategy = require('../../').Strategy;
-  const SteamAPI = require('steamapi');
-  const steam = new SteamAPI('6B328D41EE66949204BBCEBA81C3852A');
 
 
 // Passport session setup.
@@ -45,11 +43,7 @@ passport.use(new SteamStrategy({
       profile.identifier = identifier;
       return done(null, profile);
     });
-  },
-  steamID = process.nextTick,
-  steam.getUserOwnedGames(steamID).then(result => {
-      console.log(result)
-  }).catch(console.error)
+  }
 ));
 
 var app = express();
