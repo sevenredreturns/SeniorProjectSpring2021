aea74c1f82cac05ca1cf191b4dc8210a52f9e8b2
Theresa Morris <morris85@students.rowan.edu>
2021-05-06 20:31:57 -0400

    update leaderboard model

--- achievement-arena/achieve-arena/src/models/leaderboard.model.js
+++ achievement-arena/achieve-arena/src/models/leaderboard.model.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 const leaderboardSchema = new mongoose.Schema({
                                                   key   : Number,
                                                   appid : Number,
+                                                  name  : String,
                                                   scores: [{
-                                                      key   : Number,
-                                                      userid: String,
+                                                      key     : Number,
+                                                      userid  : String,
                                                       username: String,
-                                                      rank  : Number,
-                                                      score : Number
+                                                      rank    : Number,
+                                                      score   : Number
                                                   }
                                                   ]
                                               });
