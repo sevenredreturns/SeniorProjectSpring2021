68b98c40381fb4b9dfd4891de0f74fdb08acf223
Theresa Morris <morris85@students.rowan.edu>
2021-05-06 22:51:36 -0400

    spacing shenanagins

--- Steam/main.js
+++ Steam/main.js
@@ -8,26 +8,35 @@ var express = require('express')
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI('6B328D41EE66949204BBCEBA81C3852A');
 
-
-
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+    done(null, obj);
 });
 
+// Use the SteamStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier and profile), and invoke a
+//   callback with a user object.
 passport.use(new SteamStrategy({
-    returnURL: 'http://localhost:3000/auth/steam/return',
-    realm: 'http://localhost:3000/',
-    apiKey: '6B328D41EE66949204BBCEBA81C3852A'
-  },
-  function(identifier, profile, done) {
-    User.findByOpenID({ openId: identifier }, function (err, user) {
-      return done(err, user);
-    });
-  }
+                                   returnURL: 'http://localhost:3000/auth/steam/return',
+                                   realm: 'http://localhost:3000/',
+                                   apiKey: '6B328D41EE66949204BBCEBA81C3852A'
+                               },
+                               function(identifier, profile, done) {
+                                   // asynchronous verification, for effect...
+                                   process.nextTick(function () {
+
+                                       // To keep the example simple, the user's Steam profile is returned to
+                                       // represent the logged-in user.  In a typical application, you would want
+                                       // to associate the Steam account with a user record in your database,
+                                       // and return that user instead.
+                                       profile.identifier = identifier;
+                                       return done(null, profile);
+                                   });
+                               }
 ));
 
 var app = express();
@@ -37,10 +46,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.use(session({
-    secret: 'your secret',
-    name: 'name of session id',
-    resave: true,
-    saveUninitialized: true}));
+                    secret: 'your secret',
+                    name: 'name of session id',
+                    resave: true,
+                    saveUninitialized: true}));
 
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
@@ -49,34 +58,48 @@ app.use(passport.session());
 app.use(express.static(__dirname + '/../../public'));
 
 app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+    res.render('index', { user: req.user });
 });
 
 app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+    res.render('account', { user: req.user });
 });
 
 app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
+// GET /auth/steam
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Steam authentication will involve redirecting
+//   the user to steamcommunity.com.  After authenticating, Steam will redirect the
+//   user back to this application at /auth/steam/return
 app.get('/auth/steam',
-  passport.authenticate('steam'),
-  function(req, res) {
-    // The request will be redirected to Steam for authentication, so
-    // this function will not be called.
-  });
+        passport.authenticate('steam', { failureRedirect: '/' }),
+        function(req, res) {
+            res.redirect('/');
+        });
 
+// GET /auth/steam/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
 app.get('/auth/steam/return',
-  passport.authenticate('steam', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+        passport.authenticate('steam', { failureRedirect: '/' }),
+        function(req, res) {
+            res.redirect('/');
+        });
 
-  app.listen(3000);
-  function ensureAuthenticated(req, res, next) {
+app.listen(3000);
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
     res.redirect('/');
-  }
+}
--- achievement-arena/achieve-arena/src/App.js
+++ achievement-arena/achieve-arena/src/App.js
@@ -1,99 +1,105 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import React                                from 'react';
+import {BrowserRouter, Route, Switch}       from 'react-router-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import GameLeaderboard from './GameLeaderboard'
-import ProfileTry from "./ProfileTry";
-import ProfilePage from "./ProfilePage";
-import PrivateRoute    from "./PrivateRoute";
-import MakeProfile from "./MakeProfile";
-import LoginPage from "./LoginPage";
-//import UpdateUserGames from "./UpdateUserGames";
-import Home            from "./Home";
-import Leaderboard from "./Leaderboard"
-import { Layout, Menu, Breadcrumb, Dropdown, Row, Col, Divider, Card, PageHeader } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import ProfilePage                          from "./ProfilePage";
+import MakeProfile                          from "./MakeProfile";
+import LoginPage                            from "./LoginPage";
+import Home                                 from "./Home";
+import Leaderboard                          from "./Leaderboard";
+import {Dropdown, Layout, Menu, PageHeader} from 'antd';
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
 
 var userID = null;
 
-function login() {
-  userID = 1;
-  console.log(userID);
+function login()
+{
+    userID = 1;
+    console.log(userID);
 }
 
-function logout() {
-  userID = null;
-  console.log(userID);
+function logout()
+{
+    userID = null;
+    console.log(userID);
 }
 
-function loggedin() {
+function loggedin()
+{
 
-  if (userID != null) {
+    if (userID != null)
+    {
+        return (
+            <Menu>
+                <Menu.Item key="0"><a href="/ProfilePage">Profile
+                    Page</a></Menu.Item>
+                <Menu.Divider/>
+                <Menu.Item key="3"><a onClick={() =>
+                {
+                    logout();
+                }}>Logout</a></Menu.Item>
+            </Menu>
+        );
+    }
     return (
-      <Menu>
-        <Menu.Item key="0"><a href="/ProfilePage">Profile Page</a></Menu.Item>
-        <Menu.Divider />
-        <Menu.Item key="3"><a onClick={() => {logout()}}>Logout</a></Menu.Item>
-      </Menu>
-    );
-  }
-  return (
-    <Menu>
-      <Menu.Item key="4"><a href="/LoginPage">Login</a></Menu.Item>
-    </Menu>
-  );
-}
-
-function App() {
-    return (
-      <div>
-      <PageHeader title="Achievement Arena" />
-      <Layout className="header">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" style={{ alignContent: 'right' }}>
-          <Menu.Item key="4"><a href="/">Home</a></Menu.Item>
-          <Menu.Item key="5"><a href="/Leaderboard">Leaderboards</a></Menu.Item>
-
-          <Menu.Item key="6">
-            <Dropdown overlay={loggedin()} trigger={['click']}>
-            <a onClick={e => e.preventDefault()}>Profile</a>
-            </Dropdown>
-          </Menu.Item>
-          {/*<Menu.Item key="7"><a href="/UpdateUserGames">Update User Games Tester</a></Menu.Item>*/}
+        <Menu>
+            <Menu.Item key="4"><a href="/LoginPage">Login</a></Menu.Item>
         </Menu>
-      </Header>
+    );
+}
 
-      <Content style={{ padding: '0 50px', minHeight: '1000px' }}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/Leaderboard">
-            <Leaderboard />
-          </Route>
-          <Route path="/LoginPage">
-            <LoginPage />
-          </Route>
-          <Route path="/ProfilePage">
-            <ProfilePage />
-          </Route>
-          <Route path="/MakeProfile">
-            <MakeProfile />
-          </Route>
-          {/*<Route path = "/UpdateUserGames">
-            <UpdateUserGames />
-          </Route>*/}
-          <Route path="/">
-            <Home/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Achievement Arena ©2021</Footer>
-    </Layout>
-    </div>
+function App()
+{
+    return (
+        <div>
+            <PageHeader title="Achievement Arena"/>
+            <Layout className="header">
+                <Header>
+                    <div className="logo"/>
+                    <Menu theme="dark" mode="horizontal"
+                          style={{alignContent: 'right'}}>
+                        <Menu.Item key="4"><a href="/">Home</a></Menu.Item>
+                        <Menu.Item key="5"><a
+                            href="/Leaderboard">Leaderboards</a></Menu.Item>
+
+                        <Menu.Item key="6">
+                            <Dropdown overlay={loggedin()} trigger={['click']}>
+                                <a onClick={e => e.preventDefault()}>Profile</a>
+                            </Dropdown>
+                        </Menu.Item>
+                        {/*<Menu.Item key="7"><a href="/UpdateUserGames">Update User Games Tester</a></Menu.Item>*/}
+                    </Menu>
+                </Header>
+
+                <Content style={{padding: '0 50px', minHeight: '1000px'}}>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route path="/Leaderboard">
+                                <Leaderboard/>
+                            </Route>
+                            <Route path="/LoginPage">
+                                <LoginPage/>
+                            </Route>
+                            <Route path="/ProfilePage">
+                                <ProfilePage/>
+                            </Route>
+                            <Route path="/MakeProfile">
+                                <MakeProfile/>
+                            </Route>
+                            {/*<Route path = "/UpdateUserGames">
+                             <UpdateUserGames />
+                             </Route>*/}
+                            <Route path="/">
+                                <Home/>
+                            </Route>
+                        </Switch>
+                    </BrowserRouter>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>Achievement Arena
+                    ©2021</Footer>
+            </Layout>
+        </div>
     );
 }
 
